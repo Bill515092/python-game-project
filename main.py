de0b36585a63980666aa88584bb2a10a47f6574a
John Doe <johndoe@example.com>
2024-10-06 14:42:52 +0100

    Player class corrected. Now present on screen

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+class Player(pygame.sprite.Sprite):
 
-# player = pygame.Rect((300, 250, 50, 50))
+    def __init__(self, x, y, x_vel, y_vel):
+        super().__init__()
+        self.position = (x, y)
+        self.velocity = (x_vel, y_vel)
+    
+    def update(self):
+        self.postion = (self.position[0], self.velocity[0], self.position[1])
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, (255, 0 ,0), self.position, 10)
+    
+    def move_left(self):
+        self.velocity = (-1, self.velocity[1])
+
+    def move_right(self):
+        self.velocity = (1, self.velocity[1])
+
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 background = pygame.image.load('/Users/williamhutchinson/python-game-project/assets/game-background1.png')
 
-
-class Player(pygame.sprite.Sprite):
-
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-
-        img = pygame.image.load(os.path.join('assets', 'knight.png')).convert()
-        self.images.append(img)
-        self.image = self.images[0]
-        self.rect = self.image.get_rect()
+player = Player(320, 240, 0 ,0)
 
 
 def gameBackground(background):
@@ -36,25 +44,21 @@ while (gameRun):
 
     gameBackground(background)
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.move_ip(-1, 0)
-    elif key[pygame.K_d] == True:
-        player.move_ip(1, 0)
-    elif key[pygame.K_w] == True:
-        player.move_ip(0, -1)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, 1)
-
-    
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                player.move_left()
+            elif event.key == pygame.K_d:
+                player.move_right()
     
-    # gameBackground(background)
+    player.update()
+
+    player.draw(screen)
     
     pygame.display.update()
 
