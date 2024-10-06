8cf3d90162a20ce93e3f69d497d15adc7029da85
John Doe <johndoe@example.com>
2024-10-06 13:43:32 +0100

    Background imgae set up. Player class created

--- main.py
+++ main.py
@@ -7,13 +7,34 @@ SCREEN_HEIGHT = 600
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((300, 250, 50, 50))
+# player = pygame.Rect((300, 250, 50, 50))
+
+background = pygame.image.load('/Users/williamhutchinson/python-game-project/assets/game-background1.png')
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.images = []
+
+        img = pygame.image.load(os.path.join('assets', 'knight.png')).convert()
+        self.images.append(img)
+        self.image = self.images[0]
+        self.rect = self.image.get_rect()
+
+
+def gameBackground(background):
+    size = pygame.transform.scale(background, (800, 600))
+    screen.blit(size, (0, 0))
 
 
 gameRun = True
 while (gameRun):
 
     screen.fill((0, 0, 0))
+
+    gameBackground(background)
 
     pygame.draw.rect(screen, (255, 0, 0), player)
 
@@ -27,9 +48,13 @@ while (gameRun):
     elif key[pygame.K_s] == True:
         player.move_ip(0, 1)
 
+    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    
+    # gameBackground(background)
     
     pygame.display.update()
 
